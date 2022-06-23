c4c086ae30b175b1c150a11a9528227db063a229
Stephanie-Tovar <stephanie.anne.tovar@gmail.com>
2022-06-23 15:18:27 +0200

    functionality to move player

--- js/main.js
+++ js/main.js
@@ -1,1 +1,57 @@
-console.log("this is the console")
+
+class Game {
+    constructor() {
+        this.player = null;
+    }
+    start() {
+        this.player = new Player();
+        this.addEventListeners();
+    }
+    addEventListeners() {
+        document.addEventListener("keydown", function (event) {
+            if (event.key === "ArrowLeft") {
+                this.player.moveLeft();
+            }
+            else if (event.key === "ArrowRight") {
+                this.player.moveRight();
+            } else {
+                console.log("please use the arrow keys");
+            }
+        })
+    }
+}
+
+class Player {
+    constructor() {
+        this.positionX = 45;
+        this.positionY = 0;
+        this.domElement = null;
+        this.createDomElement();
+    }
+    createDomElement() {
+        //create dom element
+        this.domElement = document.createElement('div');
+
+        //set id and css
+        this.domElement.id = "player";
+        this.domElement.style.left = this.positionX + "vw";
+        this.domElement.style.bottom = this.positionY + "vh";
+
+        //append child to parent 
+        const boardElm = document.getElementById('board');
+        boardElm.appendChild(this.domElement);
+    }
+    moveLeft() {
+        this.positionX--;
+        this.domElement.style.left = this.position + "vw";
+    }
+    moveRight() {
+        this.positionX++;
+        this.domElement.style.left = this.position + "vw";
+    }
+}
+
+
+const game = new Game();
+game.start();
+
